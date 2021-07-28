b81e9b0b3051c3debd3aa59b69aea336786cf656
lauren-kohn <lauren.kohn@soft-adv.com>
2021-07-28 16:47:20 -0500

    commit tree 8931b9c3e3ef47ac86628b79ea847633400a8e8c

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,13 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |cat_attr|
+      if cat_attr["name"].present?
+        category = Category.find_or_create_by(cat_attr)
+        self.categories << category 
+      end 
+    end
+  end 
+  
 end
